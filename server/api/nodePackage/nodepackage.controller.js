882fbba6383c593456f29786f23754441a443906
David Daester <david@daester.net>
2015-04-05 01:36:41 +0200

    Speed up statistics.
    Statistic generated only if there where some schedulers running since the last update.

--- server/api/nodePackage/nodepackage.controller.js
+++ server/api/nodePackage/nodepackage.controller.js
@@ -4,6 +4,8 @@ var _ = require('lodash');
 var async = require('async');
 var Scheduler = require('../scheduler/scheduler.model');
 var NodePackage = require('./nodepackage.model.js');
+var mapReduceModelCreator = require('../../components/MapReduceSchema');
+var Setting = require('../setting/setting.model');
 
 // Get list of plugins
 exports.index = function (req, res) {
@@ -58,6 +60,7 @@ exports.byKeyword = function (req, res) {
 };
 
 exports.statistics = function (req, res) {
+  var mapReduceCollectionName = 'package_keyword_statistics';
   var mapReduceObj = {
     map: function () {
       var i = 0, length = this.keywords.length, keywords = this.keywords;
@@ -68,13 +71,11 @@ exports.statistics = function (req, res) {
     reduce: function (k, values) {
       return Array.sum(values);
     },
-    out: {replace: 'package_keyword_statistics'}
+    out: {replace: mapReduceCollectionName}
   };
-  NodePackage.mapReduce(mapReduceObj, function (err, model, stats) {
-    if (err) {
-      return handleError(res, err);
-    }
-    console.log(stats);
+
+  function sendStatistics() {
+    var model = mapReduceModelCreator(mapReduceCollectionName);
     async.parallel({
         countOfKeywords: function (cb) {
           model.count(cb);
@@ -90,7 +91,7 @@ exports.statistics = function (req, res) {
           model.count({value: {$gte: 10, $lt: 100}}, cb);
         }, countOfKeywordsBetween100And500: function (cb) {
           model.count({value: {$gte: 100, $lt: 500}}, cb);
-        }, countOfKeywordsBetween500And100: function (cb) {
+        }, countOfKeywordsBetween500And1000: function (cb) {
           model.count({value: {$gte: 500, $lt: 1000}}, cb);
         }, countOfKeywordsGreater1000: function (cb) {
           model.count({value: {$gte: 1000}}, cb);
@@ -102,8 +103,62 @@ exports.statistics = function (req, res) {
         res.json(resObj);
       }
     );
-  });
-}
+  }
+
+  function updateStats(callback) {
+    NodePackage.mapReduce(mapReduceObj, function (err, model, stats) {
+      if (err) {
+        return callback(err);
+      }
+      console.log(stats);
+      callback();
+
+    });
+  }
+
+  Setting.findOne({name: mapReduceCollectionName}, function (err, doc) {
+    function updateStatsCallback(err) {
+      if (err) {
+        return handleError(res, err);
+      }
+      sendStatistics();
+    }
+
+    if (err) {
+      return handleError(res, err);
+    }
+    if (!doc) {
+      Setting.create({
+        name: mapReduceCollectionName,
+        info: 'last statistic mapReduce update called',
+        value: new Date(),
+        editable: false
+      }, function (err) {
+        if (err) {
+          return handleError(res, err);
+        }
+        updateStats(updateStatsCallback);
+      })
+    }
+    else {
+      Scheduler.count({type: 'package', lastFinish: {$gte: doc.value}}).exec(function (err, count) {
+        if (err) {
+          return handleError(res, err);
+        }
+        if (count <= 0) {
+          sendStatistics();
+        }
+        else {
+          doc.value = new Date();
+          doc.save(function () {
+            updateStats(updateStatsCallback);
+          });
+        }
+      });
+    }
+  })
+
+};
 
 function handleError(res, err) {
   console.log(err);
