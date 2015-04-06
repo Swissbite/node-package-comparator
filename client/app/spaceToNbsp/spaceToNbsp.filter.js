75236ea3f513bd3ce25fbe836fd4661b0f26ad87
David Daester <david@daester.net>
2015-04-07 00:10:56 +0200

    UI improvement on statistics.

--- client/app/spaceToNbsp/spaceToNbsp.filter.js
+++ client/app/spaceToNbsp/spaceToNbsp.filter.js
@@ -1,10 +1,11 @@
 'use strict';
 
 angular.module('NodePackageComperatorApp')
-  .filter('spaceToNbsp', function () {
+  .filter('spaceToNbsp', function ($sanitize) {
     return function (input) {
       if (angular.isString(input)) {
-        return input.replace(/ /g, '&nbsp;');
+        console.log($sanitize(input));
+        return $sanitize(input).replace(/ /g, '&nbsp;');
       }
       return input;
     };
